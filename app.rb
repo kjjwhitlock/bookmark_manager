1745aa09d2331942521a8a56ec4f1e28ff3be62d
charlierdm <charlie.davismiller@gmail.com>
2021-02-24 16:52:08 +0000

    added post request + submit url via SQL

--- app.rb
+++ app.rb
@@ -3,15 +3,26 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
-get '/' do
-  'Hello'
-end
+  get '/' do
+    'Hello'
+  end
 
-get '/bookmarks' do
-  @list = Bookmark.all
-  erb :index
-end
+  get '/bookmarks' do
+    @list = Bookmark.all
+    erb :index
+  end
 
-run! if app_file == $0
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    url = params[:url]
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end
+
+  run! if app_file == $0
 
 end
